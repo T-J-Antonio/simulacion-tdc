cc99f088a988db8857c5c7f76954e13432f86fbd
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-12-06 20:52:29 -0300

    add experimental gui

---
+++ simulacion_gui.py
@@ -0,0 +1,123 @@
+import tkinter as tk
+from tkinter import simpledialog, messagebox
+import matplotlib.pyplot as plt
+import random
+
+def run_simulation(valor_nominal, perturbaciones):
+    def generate_perturbacion(t):
+        return perturbaciones[t] if t in perturbaciones else 0
+
+    def umbrales(error):
+        abs_error = abs(error)
+        correction = 0
+        if abs_error < 10:
+            correction = 0
+        elif 10 <= abs_error < 20:
+            correction = 5
+        elif 20 <= abs_error < 30:
+            correction = 10
+        elif 30 <= abs_error < 40:
+            correction = 15
+        else:
+            correction = 50
+        return -correction if error > 0 else correction
+
+    estados_sv = []
+    porcentajes_estado_sv = []
+    tiempos = []
+    perturbaciones_list = []
+    valores_e = []
+    valores_f = []
+
+    estado_del_servidor = 0
+    error_anterior = 0
+    Kd = 0.4
+
+    for i in range(1000):
+        tiempos.append(i)
+        variacion = random.randint(-50, +50)
+        estado_del_servidor += variacion
+
+        valor_real = estado_del_servidor
+        valores_f.append(variacion)
+
+        señal_error = valor_nominal - valor_real
+        valores_e.append(señal_error)
+
+        salida_proporcional = umbrales(señal_error) * -1
+        salida_derivativo = Kd * (señal_error - error_anterior)
+        error_anterior = señal_error
+
+        salida_total_controlador = salida_proporcional + salida_derivativo
+        estados_sv.append(estado_del_servidor)
+        porcentajes_estado_sv.append(
+            round(estado_del_servidor / valor_nominal * 100, 2))
+        salida_rl = salida_total_controlador
+
+        perturbacion = generate_perturbacion(i)
+        perturbaciones_list.append(perturbacion)
+
+        estado_del_servidor += salida_rl + perturbacion
+
+    plt.figure(figsize=(20, 12))
+
+    plt.subplot(3, 2, 1)
+    plt.plot(tiempos, estados_sv, label='Requests por minuto')
+    plt.axhline(y=valor_nominal, color='r', linestyle='--', label='Valor nominal')
+    plt.xlabel('Tiempo (min)')
+    plt.ylabel('Requests/min')
+    plt.legend()
+    plt.grid(True)
+
+    plt.subplot(3, 2, 2)
+    plt.plot(tiempos, porcentajes_estado_sv, label='Porcentaje sobre valor nominal')
+    plt.xlabel('Tiempo (min)')
+    plt.ylabel('Porcentaje de requests')
+    plt.legend()
+    plt.grid(True)
+
+    plt.subplot(3, 2, 3)
+    plt.plot(tiempos, perturbaciones_list)
+    plt.xlabel('Tiempo (min)')
+    plt.ylabel('Perturbaciones')
+    plt.grid(True)
+
+    plt.subplot(3, 2, 4)
+    plt.plot(tiempos, valores_e)
+    plt.xlabel('Tiempo (min)')
+    plt.ylabel('Errores')
+    plt.grid(True)
+
+    plt.subplot(3, 2, 5)
+    plt.plot(tiempos, valores_f)
+    plt.xlabel('Tiempo (min)')
+    plt.ylabel('Mediciones')
+    plt.grid(True)
+
+    plt.tight_layout()
+    plt.show()
+
+def main():
+    while True:
+        root = tk.Tk()
+        root.withdraw()
+
+        valor_nominal = simpledialog.askinteger("Input", "Enter valor_nominal:", minvalue=1)
+        if valor_nominal is None:
+            break
+
+        perturbaciones = {}
+        while True:
+            perturb_time = simpledialog.askinteger("Input", "Enter perturbation time (or -1 to finish):", minvalue=-1)
+            if perturb_time == -1 or perturb_time is None:
+                break
+            perturb_value = simpledialog.askinteger("Input", "Enter perturbation value:")
+            perturbaciones[perturb_time] = perturb_value
+
+        run_simulation(valor_nominal, perturbaciones)
+
+        if not messagebox.askyesno("Continue", "Do you want to run another simulation?"):
+            break
+
+if __name__ == "__main__":
+    main()
